0b3c066271b3f8e360686ba7595f1bf52f86a720
yash gondaliya <yashgondaliya666@gmail.com>
2024-09-02 18:35:04 +0530

    product delete and show

--- src/pages/adminPanel/allProducts/AllProducts.jsx
+++ src/pages/adminPanel/allProducts/AllProducts.jsx
@@ -1,7 +1,106 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styles from "../../../styles/pages/adminPanel/allProducts/allProducts.module.css";
 
 function AllProducts() {
-  return <div>AllProducts</div>;
+  const [allProductsState, setAllProductsState] = useState([]);
+
+  useEffect(() => {
+    allProducts();
+  }, []);
+
+  async function allProducts() {
+    try {
+      const response = await axios.get("http://localhost:8000/admin/products", {
+        withCredentials: true,
+      });
+      setAllProductsState(response.data.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }
+
+  async function handleDeleteProducts(id) {
+    try {
+      const response = await axios.delete(
+        `http://localhost:8000/admin/products/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+      await allProducts();
+      alert(response.data.message);
+    } catch (error) {
+      console.error("Error deleting user:", error);
+    }
+  }
+
+  function handleEditProducts(id) {
+    console.log(id);
+  }
+
+  if (!allProductsState) return null;
+
+  return (
+    <div className={styles.container}>
+      <h1>All Products</h1>
+      <ul className={styles.productList}>
+        {allProductsState?.map((product) => (
+          <li key={product?._id} className={styles.productItem}>
+            <img
+              src={product?.img[0]}
+              alt={product?.productName}
+              className={styles.productImage}
+            />
+            <h2 className={styles.productName}>{product?.productName}</h2>
+            <p className={styles.productDetail}>
+              <strong>Description:</strong> {product?.desc}
+            </p>
+            <p className={styles.productDetail}>
+              <strong>Price:</strong> ${product?.price}
+            </p>
+            <p className={styles.productDetail}>
+              <strong>Discount:</strong> {product?.discount}%
+            </p>
+            <p className={styles.productDetail}>
+              <strong>Final Price:</strong> ${product?.finalPrice}
+            </p>
+            <p className={styles.productDetail}>
+              <strong>Size:</strong> {product?.size?.join(", ")}
+            </p>
+            <p className={styles.productDetail}>
+              <strong>Stock:</strong> {product?.stock}
+            </p>
+            <p className={styles.productDetail}>
+              <strong>Category:</strong> {product?.category?.name}
+            </p>
+            <p className={styles.productDetail}>
+              <strong>Sub-category:</strong>{" "}
+              {product?.subCategory?.subCategoryName}
+            </p>
+            <div className={styles.buttonContainer}>
+              <button
+                className={styles.editButton}
+                onClick={() => {
+                  handleEditProducts(product._id);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className={styles.deleteButton}
+                onClick={() => {
+                  handleDeleteProducts(product._id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default AllProducts;
--- src/pages/adminPanel/allProducts/CreateProducts.jsx
+++ src/pages/adminPanel/allProducts/CreateProducts.jsx
@@ -1,13 +1,58 @@
 import React, { useEffect, useState, useCallback } from "react";
 import styles from "../../../styles/pages/adminPanel/allProducts/createProducts.module.css";
 import axios from "axios";
+import cloudinaryImages from "../../../helper/cloudinary";
 
 function CreateProducts() {
   const [categories, setCategories] = useState([]);
   const [subCategories, setSubCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedSubCategory, setSelectedSubCategory] = useState("");
-//   const [productName, setProductName] = useState("");
+  const [sizes, setSizes] = useState([
+    "1-2Y",
+    "2-3Y",
+    "3-4Y",
+    "4-5Y",
+    "5-6Y",
+    "6-7Y",
+    "7-8Y",
+    "8-9Y",
+    "9-10Y",
+    "XXS",
+    "XS",
+    "S",
+    "M",
+    "L",
+    "XL",
+    "XXL",
+    "XXXL",
+    "28",
+    "30",
+    "32",
+    "34",
+    "36",
+    "38",
+    "40",
+    "UK 6",
+    "UK 7",
+    "UK 8",
+    "UK 9",
+    "UK 10",
+    "UK 11",
+  ]);
+  const [selectedSizes, setSelectedSizes] = useState([]);
+  const [product, setProduct] = useState({
+    productName: "",
+    size: [],
+    img: [],
+    desc: "",
+    price: "",
+    discount: "",
+    category: "",
+    subCategory: "",
+    finalPrice: "",
+    stock: "",
+  });
 
   const fetchCategories = useCallback(async () => {
     try {
@@ -51,41 +96,112 @@ function CreateProducts() {
     const newCategory = e.target.value;
     setSelectedCategory(newCategory);
     setSelectedSubCategory("");
+    setProduct((prev) => ({ ...prev, category: newCategory }));
   };
 
   const handleSubCategoryChange = (e) => {
-    setSelectedSubCategory(e.target.value);
+    const newSubCategory = e.target.value;
+    setSelectedSubCategory(newSubCategory);
+    setProduct((prev) => ({ ...prev, subCategory: newSubCategory }));
   };
 
-//   async function handleProduct(e) {
-//     e.preventDefault();
-//     try {
-//       await axios.post(
-//         "http://localhost:8000//admin/products/createProduct",
-//         {
-//           productName: productName,
-//           categoryId: selectedCategory,
-//         },
-//         { withCredentials: true }
-//       );
-//       alert("Subcategory created successfully!");
-//       setProductName("");
-//     } catch (error) {
-//       console.log(error);
-//       alert("error");
-//     }
-//   }
+  const handleProductChange = (e) => {
+    const { name, value } = e.target;
+    setProduct((prev) => ({ ...prev, [name]: value }));
+  };
 
-//   function handleProductChange(e) {
-//     setProductName(e.target.value);
-//   }
+  const handleSizeChange = (size) => {
+    setSelectedSizes((prev) => {
+      const isSelected = prev.includes(size);
+      if (isSelected) {
+        return prev.filter((s) => s !== size);
+      } else {
+        return [...prev, size];
+      }
+    });
+  };
 
-  console.log(selectedSubCategory);
+  useEffect(() => {
+    setProduct((prev) => ({ ...prev, size: selectedSizes }));
+  }, [selectedSizes]);
+
+  const validate = () => {
+    const newErrors = {};
+    for (const key in product) {
+      if (
+        product[key] === "" &&
+        [
+          "productName",
+          "size",
+          "img",
+          "desc",
+          "price",
+          "discount",
+          "category",
+          "subCategory",
+          "finalPrice",
+          "stock",
+        ].includes(key)
+      ) {
+        newErrors[key] = "This field is required";
+      }
+    }
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleProduct = async (e) => {
+    e.preventDefault();
+    if (!validate()) return null;
+
+    try {
+      await axios.post(
+        "http://localhost:8000/admin/products/createProduct",
+        {
+          ...product,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      alert("Product created successfully");
+      setProduct({
+        productName: "",
+        size: [],
+        img: [],
+        desc: "",
+        price: "",
+        discount: "",
+        category: "",
+        subCategory: "",
+        finalPrice: "",
+        stock: "",
+      });
+      setSelectedSizes([]);
+    } catch (error) {
+      console.log(error);
+      alert("Error creating product");
+    }
+  };
+
+  function handleChange(e) {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  }
+
+  async function handleUploadPic(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const imagePic = await cloudinaryImages(file);
+      setProduct((prev) => ({
+        ...prev,
+        img: imagePic.url,
+      }));
+    }
+  }
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Select Category and Subcategory</h1>
-      <form className={styles.form}>
+      <h1 className={styles.title}>Create a New Product</h1>
+      <form className={styles.form} onSubmit={handleProduct}>
         <div>
           <label htmlFor="category">Category:</label>
           <select
@@ -124,17 +240,130 @@ function CreateProducts() {
           </div>
         )}
 
-        {/* {selectedSubCategory && (
-          <form onSubmit={handleProduct}>
-            <input
-              className={styles.input}
-              value={productName}
-              onChange={handleProductChange}
-              placeholder="Enter subcategory name"
-            />
-            <button className={styles.button}>Create Subcategory</button>
-          </form>
-        )} */}
+        {selectedSubCategory && (
+          <div>
+            <div>
+              <label htmlFor="productName">Product Name:</label>
+              <input
+                name="productName"
+                id="productName"
+                className={styles.input}
+                value={product.productName}
+                onChange={handleProductChange}
+                placeholder="Enter product name"
+              />
+            </div>
+
+            <div>
+              <label>Size:</label>
+              <div className={styles.sizeOptions}>
+                {sizes.map((size) => (
+                  <div key={size}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={selectedSizes.includes(size)}
+                        onChange={() => handleSizeChange(size)}
+                      />
+                      {size}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className={styles.imgCon}>
+              <div className={styles.imgInnerCon}>
+                <div className={styles.imgInnerCon}>
+                  {product.img ? (
+                    <img src={product.img} alt="Profile Preview" />
+                  ) : (
+                    <div className={styles.imgPlaceholder}>No Image</div>
+                  )}
+                </div>
+              </div>
+              <form>
+                <label>
+                  <div className={styles.uploadPhoto}>Upload Photo</div>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    style={{ display: "none" }}
+                    onChange={handleUploadPic}
+                  />
+                </label>
+              </form>
+            </div>
+
+            <div>
+              <label htmlFor="desc">Description:</label>
+              <textarea
+                name="desc"
+                id="desc"
+                className={styles.input}
+                value={product.desc}
+                onChange={handleProductChange}
+                placeholder="Enter description"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="price">Price:</label>
+              <input
+                name="price"
+                id="price"
+                className={styles.input}
+                type="number"
+                value={product.price}
+                onChange={handleProductChange}
+                placeholder="Enter price"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="discount">Discount:</label>
+              <input
+                name="discount"
+                id="discount"
+                className={styles.input}
+                type="number"
+                value={product.discount}
+                onChange={handleProductChange}
+                placeholder="Enter discount"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="finalPrice">Final Price:</label>
+              <input
+                name="finalPrice"
+                id="finalPrice"
+                className={styles.input}
+                type="number"
+                value={product.finalPrice}
+                onChange={handleProductChange}
+                placeholder="Enter final price"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="stock">Stock:</label>
+              <input
+                name="stock"
+                id="stock"
+                className={styles.input}
+                type="number"
+                value={product.stock}
+                onChange={handleProductChange}
+                placeholder="Enter stock"
+              />
+            </div>
+
+            <button className={styles.button} type="submit">
+              Create Product
+            </button>
+          </div>
+        )}
       </form>
     </div>
   );
